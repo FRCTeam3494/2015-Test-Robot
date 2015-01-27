6ef1144b84914f9f714308348f097ee7850fb84c
MisguidedBadge <petersontrey@hotmail.com>
2015-01-26 16:55:43 -0800

    new comments and cleanup and accelerometers

--- Mecanum Drive/src/Robot.cpp
+++ Mecanum Drive/src/Robot.cpp
@@ -10,39 +10,66 @@ class Robot: public SampleRobot
 {
 
     // Channels for the wheels
-    const static int frontLeftChannel	= 2;
+    const static int frontLeftChannel	= 2;							// Default values for each channel
     const static int rearLeftChannel	= 3;
     const static int frontRightChannel	= 1;
     const static int rearRightChannel	= 0;
 
-    const static int joystickChannel	= 0;
+    const static int joystickChannel	= 0;							// default joystick channel
+
+    double Acc_Y = 0; 													// These are the accelerometer values used in the roboRIO
+    double Acc_X = 0; 													//
+    double Acc_Z = 0; 													//
+
 
     //SmartDashboard* Smart;
-    Can_Data* Can;
-    CANTalon Talon_1;
-    Encoder encoder_1;
-	RobotDrive robotDrive;	// robot drive system
-	Joystick stick; // only joystick
-	bool Period = 0;
+    BuiltInAccelerometer* accelerometer_1;
 
-	double Period_2 = 0;
+    Can_Data* Can; 														// Can data
+
+    CANTalon Talon_1; 										    		// CAN Talon soon to change
+
+    Encoder* encoder_1; 													// Encoder initiliazed
+
+	RobotDrive* robotDrive;												// robot drive system
+
+	Joystick* stick; 													// only joystick
+
+
+	double Period_2 = 0; 												// Encoder value puller prototype
 
 public:
-	Robot() : encoder_1(0,1,false, Encoder::k1X),
-			robotDrive(frontLeftChannel, rearLeftChannel,
-					   frontRightChannel, rearRightChannel),	// these must be initialized in the same order
-			stick(joystickChannel)				// as they are declared above.
+	Robot()
+
+							                                            // as they are declared above.
 	{
-		robotDrive.SetExpiration(0.1);
-		robotDrive.SetInvertedMotor(RobotDrive::kFrontLeftMotor, true);	// invert the left side motors
-		robotDrive.SetInvertedMotor(RobotDrive::kRearLeftMotor, true);	// you may need to change or remove this to match your robot
-		Can = new Can_Data;
-SmartDashboard::init();
-		encoder_1.SetSamplesToAverage(5);
-		encoder_1.SetDistancePerPulse(1.00 / 360.0 * 2.0 *3.1415 * 1.5);
-		encoder_1.SetMinRate(1.0);
-		encoder_1.SetReverseDirection(true);
-		encoder_1.SetMaxPeriod(1);
+		robotDrive = new RobotDrive(frontLeftChannel, rearLeftChannel,frontRightChannel, rearRightChannel);
+
+		robotDrive->SetExpiration(0.1);
+
+
+
+		robotDrive->SetInvertedMotor(RobotDrive::kFrontLeftMotor, true); // invert the left side motors
+
+		robotDrive->SetInvertedMotor(RobotDrive::kRearLeftMotor, true);  // you may need to change or remove this to match your robot
+
+		Can = new Can_Data;                                             // Initializing Can
+
+		stick = new Joystick(0);											// Initializing joystick
+
+		accelerometer_1 = new BuiltInAccelerometer;						// Initializing accelerometer_1
+
+		encoder_1 = new Encoder;										// Initialize encoder
+
+
+SmartDashboard::init();                                                 // Initializing Smart Dashboard
+
+		encoder_1(0,1,false, Encoder::k1X),
+		encoder_1->SetSamplesToAverage(5); 								 // Encoder settings and parameters
+		encoder_1->SetDistancePerPulse(1.00 / 360.0 * 2.0 *3.1415 * 1.5); //
+		encoder_1->SetMinRate(1.0); 										 //
+		encoder_1->SetReverseDirection(true);							 //
+		encoder_1->SetMaxPeriod(1);										 //
 
 
 	}
@@ -57,20 +84,28 @@ SmartDashboard::init();
 		robotDrive.SetSafetyEnabled(false);
 		while (IsOperatorControl() && IsEnabled())
 		{
-        	// Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
-        	// This sample does not use field-oriented drive, so the gyro input is set to zero.
-			robotDrive.MecanumDrive_Cartesian(stick.GetX(), stick.GetY(), stick.GetZ());
 
-			Wait(0.005); // wait 5ms to avoid hogging CPU cycles
+			robotDrive->MecanumDrive_Cartesian(stick.GetX(), stick.GetY(), stick.GetZ()); // subject to change
+
+			Wait(0.005); 																 // wait 5ms to avoid hogging CPU cycles
+
+			Acc_Y = accelerometer_1->GetY();											 // Pulling Accelerometer values
+			Acc_X = accelerometer_1->GetX();											 //
+			Acc_Z = accelerometer_1->GetZ();											 //
 
 
-			Period_2 = encoder_1.Get();
+			Period_2 = encoder_1.Get();                                                  // Encoder values are being pulled
+
 
 			if (stick.GetRawButton(1) == TRUE)     // Take out of if statement next !!!!!!!!!!!!!!!!!!
 			{
 
-				SmartDashboard::PutNumber( "Encoder" , Period_2 );
-						}
+				SmartDashboard::PutNumber( "Encoder" , Period_2 );						 // Pushing values to smartdashboard
+				SmartDashboard::PutNumber( "Accelerometer_Y_Axis" , Acc_Y);				 //
+
+
+
+			}
 			Can->updateData();
 		}
 
